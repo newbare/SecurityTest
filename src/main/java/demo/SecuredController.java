9e915e8fcdb69fca380e27d673b77f6e0df0a05c
ian_lin <ian_lin@gemteks.com>
2015-03-16 17:22:21 +0800

    [master] First commit

---
+++ src/main/java/demo/SecuredController.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecuredController {
+
+	@RequestMapping("/user")
+	public User getUser(@RequestParam("name") String userName, @RequestParam("password") String userPassword) {
+		return new User(userName, userPassword);
+	}
+}
