31e8fa3bf5c431de7558e46c82a136c8724f141a
Stephen <sevenlol@sevenlol-Aspire-S7-391.(none)>
2015-03-27 20:50:14 +0800

    [master] Modify comment functions

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -15,11 +15,24 @@ eCharmApp.config(['$routeProvider', '$httpProvider',
 				templateUrl : 'partials/test.html',
 				controller  : 'testController'
 			}).
-			when('/articleList', {
-				templateUrl : 'partials/article-list.html'
+			when('/articleList/:category', {
+				templateUrl : 'partials/article-list.html',
+				controller  : 'articleListController'
 			}).
-			when('/article', {
-				templateUrl : 'partials/article.html'
+			when('/article/:category/:articleId', {
+				templateUrl : 'partials/article.html',
+				controller  : 'articleController'
+			}).
+			when('/createArticle/:category', {
+				templateUrl : 'partials/create-article.html',
+				controller  : 'createArticleController'
+			}).
+			when('/updateArticle/:category/:articleId', {
+				templateUrl : 'partials/edit-article.html',
+				controller  : 'updateArticleController'
+			}).
+			when('/userList/:userType', {
+				templateUrl : 'partials/user-list.html'
 			}).
 			otherwise({
 				redirectTo : '/test'
@@ -27,6 +40,10 @@ eCharmApp.config(['$routeProvider', '$httpProvider',
 
 		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 	}]);
+
+eCharmApp.run(function($rootScope) {
+	$rootScope.serverUrl = "https://localhost:9000/";
+});
 
 eCharmApp.controller('authController',
 	function($scope, $rootScope, $http, $location) {
@@ -108,4 +125,232 @@ eCharmApp.controller("testController",
 				$scope.fail = true;
 			});
 		}
+	});
+
+eCharmApp.controller("articleListController",
+	function($scope, $rootScope, $routeParams, $http, $location, $route) {
+		$scope.success = false;
+		$scope.fail = false;
+		$scope.category = $routeParams.category;
+		$scope.articleList = [];
+		$scope.errorStatus = -1;
+
+		$scope.deleteArticle = function(category, articleId) {
+			if (category == null || articleId == null)
+				return;
+
+			$http.delete($rootScope.serverUrl + 'articles/' + category + "/" + articleId).success(function(data, status, headers, config) {
+				$location.path("/articleList/" + category);
+			}).error(function(data, status, headers, config) {
+				// delete error
+				alert("Delete-article failed. HTTP Status: " + status)
+			});
+		}
+
+		$scope.deleteArticleInCategory = function() {
+			$http.delete($rootScope.serverUrl + 'articles/' + $scope.category).success(function(data, status, headers, config) {
+				$route.reload();
+			}).error(function(data, status, headers, config) {
+				// delete error
+				alert("Delete-category failed. HTTP Status: " + status)
+			});
+		};
+
+		$scope.deleteAllArticle = function() {
+			$http.delete($rootScope.serverUrl + 'articles').success(function(data, status, headers, config) {
+				$route.reload();
+			}).error(function(data, status, headers, config) {
+				// delete error
+				alert("Delete-all failed. HTTP Status: " + status)
+			});
+		};
+
+		var readArticleInCategory = function() {
+			$http.get($rootScope.serverUrl + 'articles/' + $scope.category).success(function(data, status, headers, config) {
+				$scope.success = true;
+				if (data && data.length > 0) {
+					$scope.articleList = data;
+				}
+			}).error(function(data, status, headers, config) {
+				$scope.fail = true;
+				$scope.errorStatus = status;
+			});
+		};
+
+		var readAllArticle = function() {
+			$http.get($rootScope.serverUrl + 'articles').success(function(data, status, headers, config) {
+				$scope.success = true;
+				if (data && data.length > 0) {
+					$scope.articleList = data;
+				}
+			}).error(function(data, status, headers, config) {
+				$scope.fail = true;
+				$scope.errorStatus = status;
+			});
+		};
+
+		if ($scope.category === "all")
+			readAllArticle();
+		else
+			readArticleInCategory();
+	});
+
+eCharmApp.controller('articleController',
+	function($scope, $rootScope, $routeParams, $http, $location, $route, $filter) {
+		$scope.articleService = {
+			success : false,
+			faile   : false,
+			errorStatus : -1
+		};
+		$scope.category = $routeParams.category;
+		$scope.articleId = $routeParams.articleId;
+		$scope.article = {};
+
+		$scope.commentService = {
+			success : false,
+			faile   : false,
+			errorStatus : -1
+		};
+		$scope.commentList = [];
+		$scope.comment = {
+			commenter_id : "test_commenter_id",
+			author_response_text : "default_response_text",
+			responded_at : "respond time"
+		};
+
+		var readArticle = function() {
+			$http.get($rootScope.serverUrl + 'articles/' + $scope.category + "/" + $scope.articleId
+			 ).success(function(data, status, headers, config) {
+				$scope.articleService.success = true;
+				if (data) {
+					$scope.article = data;
+				}
+			}).error(function(data, status, headers, config) {
+				$scope.articleService.fail = true;
+				$scope.articleService.errorStatus = status;
+			});
+		};
+
+		var readCommentList = function() {
+			$http.get($rootScope.serverUrl + 'articles/' + $scope.category + "/" +
+				      $scope.articleId + "/comments"
+			 ).success(function(data, status, headers, config) {
+				$scope.commentService.success = true;
+				if (data && data.length > 0) {
+					$scope.commentList = data;
+				}
+			}).error(function(data, status, headers, config) {
+				$scope.commentService.fail = true;
+				$scope.commentService.errorStatus = status;
+			});
+		};
+
+		$scope.createComment = function() {
+			var date = new Date();
+			var parsedDate = $filter('date')(date, 'yyyy-MM-dd HH:mm:ss Z', '+0800');
+			$scope.comment.created_at = parsedDate;
+			$scope.comment.updated_at = parsedDate;
+			
+			$http.post($rootScope.serverUrl + 'articles/' + $scope.category + "/" +
+				      $scope.articleId + "/comments", $scope.comment)
+			.success(function(data, status, headers, config) {
+				$route.reload();
+			}).error(function(data, status, headers, config) {
+				alert("Create Comment Failed (HTTP Status: " + status + ")");
+			});
+		};
+
+		readArticle();
+		readCommentList();
+	});
+
+eCharmApp.controller('createArticleController',
+	function($scope, $rootScope, $routeParams, $http, $location, $filter) {
+		$scope.success = false;
+		$scope.fail = false;
+		$scope.category = $routeParams.category;
+		$scope.errorStatus = -1;
+
+		if ($scope.category === 'default')
+			$scope.category = 'Category_1';
+
+		$scope.article = {
+			title : 'Title',
+			author_id : 'Author ID',
+			content_text : 'Content',
+			image_arr : [],
+			tag_arr : [],
+			rating : 0,
+			rating_count : 0
+		};
+
+		$scope.createArticle = function() {
+			var date = new Date();
+			var parsedDate = $filter('date')(date, 'yyyy-MM-dd HH:mm:ss Z', '+0800');
+			$scope.article.created_at = parsedDate;
+			$scope.article.updated_at = parsedDate;
+
+			$http.post($rootScope.serverUrl + 'articles/' + $scope.category, $scope.article
+			 ).success(function(data, status, headers, config) {
+				$scope.success = true;
+				alert("Create Article Successfully!");
+				$location.path("/articleList/" + $scope.category);
+			}).error(function(data, status, headers, config) {
+				$scope.fail = true;
+				$scope.errorStatus = status;
+			});
+		};
+	});
+
+eCharmApp.controller('updateArticleController',
+	function($scope, $rootScope, $routeParams, $http, $location, $filter) {
+		$scope.success = false;
+		$scope.fail = false;
+		$scope.category = $routeParams.category;
+		$scope.articleId = $routeParams.articleId;
+		$scope.article = {};
+		$scope.errorStatus = -1;
+
+		var readArticle = function() {
+			$http.get($rootScope.serverUrl + 'articles/' + $scope.category + "/" + $scope.articleId
+			 ).success(function(data, status, headers, config) {
+				$scope.success = true;
+				if (data) {
+					$scope.article = data;
+				}
+			}).error(function(data, status, headers, config) {
+				$scope.fail = true;
+				$scope.errorStatus = status;
+			});
+		}
+
+		$scope.updateArticle = function() {
+			$http.put($rootScope.serverUrl + 'articles/' + $scope.category + "/" + $scope.article.article_id, $scope.article
+			 ).success(function(data, status, headers, config) {
+				$scope.success = true;
+				if (data) {
+					$scope.article = data;
+				}
+				$location.path("/article/" + $scope.category + "/" + $scope.article.article_id);
+			}).error(function(data, status, headers, config) {
+				$scope.fail = true;
+				$scope.errorStatus = status;
+			});
+		};
+
+		$scope.partiallyUpdateArticle = function() {
+			$http.patch($rootScope.serverUrl + 'articles/' + $scope.category + "/" + $scope.article.article_id, $scope.article
+			 ).success(function(data, status, headers, config) {
+				$scope.success = true;
+				if (data) {
+					$scope.article = data;
+				}
+				$location.path("/article/" + $scope.category + "/" + $scope.article.article_id);
+			}).error(function(data, status, headers, config) {
+				$scope.fail = true;
+				$scope.errorStatus = status;
+			});
+		};
+
+		readArticle();
 	});
